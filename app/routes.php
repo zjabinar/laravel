ed39f02f3807a0e95cb1d264a47a8ed37278cb7d
Zaldy Jabinar <zjabinar@yahoo.com>
2014-04-06 15:57:39 +0800

    Update Routing

--- app/routes.php
+++ app/routes.php
@@ -11,10 +11,6 @@
 |
 */
 
-Route::get('login', array('uses' => 'UserController@showLogin'));
-Route::post('login', array('uses' => 'UserController@doLogin'));
-Route::get('logout', array('uses' => 'UserController@doLogout'));
-
 Route::get('/', function()
 {
 	return View::make('users.main');
@@ -45,5 +41,51 @@ Route::post('email_send', function()
 Route::resource('users', 'UserController');
 
 
+Route::get('login', array('uses' => 'UserController@showLogin'));
+//Route::post('login', array('uses' => 'UserController@doLogin'));
 
 
+Route::get('logout', array('uses' => 'UserController@doLogout'));
+
+
+Route::post('login', function()
+{
+	// validate the info, create rules for the inputs
+			$rules = array(
+				'username'    => 'required', // make sure the email is an actual email
+				'password' => 'required|alphaNum|min:3' // password can only be alphanumeric and has to be greater than 3 characters
+			);
+
+			// run the validation rules on the inputs from the form
+			$validator = Validator::make(Input::all(), $rules);
+		
+			// if the validator fails, redirect back to the form
+			if ($validator->fails()) {
+
+				return Redirect::to('login')
+					->withErrors($validator) // send back all errors to the login form
+					->withInput(Input::except('username')); // send back the input (not the password) so that we can repopulate the form
+			} else {
+
+				// create our user data for the authentication
+				$userdata = array(
+					'username' 	=> Input::get('username'),
+					'password' 	=> Input::get('password')
+				);
+
+				// attempt to do the login
+				if (Auth::attempt($userdata)) {
+
+					// validation successful!
+					// redirect them to the secure section or whatever
+					return View::make('users.private');
+				} else {	 	
+
+					// validation not successful, send back to form	
+					return Redirect::to('login');
+
+				}
+
+			}
+	
+});
